a9746a8698234ab7a2b40cf34968de00272e6ece
Josh Feldman <joshfeldman500@gmail.com>
2023-03-22 14:39:53 -0400

    array map lab done

--- index.js
+++ index.js
@@ -10,7 +10,34 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+// adjustedTitles=[]
+// for (const title of tutorials) {
+//   adjustedTitles.push(title);
+// }
+// const titleCased =()=> 
+//   {
+//     adjustedTitles=[]
+//     for for (const title of tutorials)
+//   return title.case()
+//   }
+
+
+// title) => 
+//   fortitle in tutorials){
+
+//   }
+//   return tutorials
+// }
+// const titleCased = map(tutorials, function (title) {
+//   return Object.assign({}, title, { accessLevel: "admin" });
+// });
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    let words=title.split(' ')
+    let capitalizedWords=words.map(word =>word.charAt(0).toUpperCase()+ word.slice(1))
+    let result = capitalizedWords.join(' ')
+    return result
+    }
+  )
 }
